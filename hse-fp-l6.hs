e05ede5b924f99301290ba6fae425aedaa933995
Evgeny Dashkov <edashkov@gmail.com>
2022-11-28 03:29:04 +0300

    add Lecture 6 and Seminar 5

---
+++ hse-fp-l6.hs
@@ -0,0 +1,458 @@
+import Control.Monad
+import Data.Functor.Contravariant
+import Data.Bifunctor
+import Control.Applicative hiding (ZipList(..))
+
+{-- Categories and the 'category' of types
+    (general definitions and discussion)
+ --}
+
+{-- Functors --}
+
+{-- Functors in categories; a functor \P : Set -> Set --}
+
+--class Functor (f :: * -> *) where
+--  fmap :: (a -> b) -> f a -> f b
+--
+-- These laws are required but not enforced:
+--  fmap id = id
+--  fmap (g . h) = fmap g . fmap h
+
+--instance Functor [] where
+-- fmap = map
+
+-- What is the kind of []?
+
+data Tree a = Nil | Node (Tree a) a (Tree a)
+    deriving Show
+
+instance Functor Tree where
+-- fmap :: (a -> b) -> (Tree a -> Tree b)
+    fmap g Nil = Nil
+    fmap g (Node l x r) = Node (fmap g l) (g x) (fmap g r)
+
+myTree = Node (Node (Node Nil 33 (Node Nil 57 Nil)) 43 (Node Nil 62 Nil))
+    74 (Node Nil 101 Nil) :: Tree Int
+
+tr1 = fmap (+1) myTree
+
+--instance Functor Maybe where
+--  fmap _ Nothing = Nothing
+--  fmap g (Just x) = Just (g x)
+
+sqhead :: [Int] -> Maybe Int
+sqhead xs = fmap (^2) $ if null xs then Nothing else Just (head xs)
+
+-- notice the kind of Either a
+--instance Functor (Either a) where
+--  fmap :: (b -> c) -> Either a b -> Either a c
+--  fmap g (Left x) = Left x
+--  fmap g (Right y) = Right (g y)
+
+f :: Int -> Either String Int
+f n = if n == 0 then Left "Zero!" else Right n
+
+g = fmap (+1) . f
+
+--instance Functor ((->) a) where
+---- NOTICE that (->) is a prefix operator,
+---- so (->) x y means x -> y.
+--  fmap :: (b -> c) -> (a -> b) -> (a -> c)
+--  fmap  =  (.)
+
+n1 = fmap (*3) (+100) 2
+n1' = (*3) . (+100) $ 2
+n2 = fmap (+1) (^3) 2
+
+--instance Functor IO where
+---- fmap :: (a -> b) -> IO a -> IO b
+--fmap g act = act >>= return . g
+
+t1 = getLine >>= return . reverse
+t2 = fmap reverse getLine
+
+
+x1 = fmap (replicate 3) "abc"
+--x2 = fmap (replicate 3) 1
+x3 = fmap (replicate 3) (Just 1)
+x4 = fmap (replicate 3) Nothing
+x5 = fmap (replicate 3) (Left "abc")
+x6 = fmap (replicate 3) (Right 1)
+x7 = fmap (replicate 3) (+1) 5
+x8 = fmap (replicate 3) getChar
+--
+
+x9 = fmap (+) [1,2,3]
+
+data Lst a = Nl | Apd a (Lst a) deriving (Show)
+
+z = Apd 1 (Apd 2 (Apd 3 Nl))
+
+{-- Functor laws do not hold here. --}
+instance Functor Lst where
+    fmap g Nl = Nl
+    fmap g (Apd x Nl) = Apd (g x) Nl
+    fmap g (Apd x (Apd y xs)) = Apd (g y) (Apd (g x) (fmap g xs))
+
+z1 = fmap id z
+z2 = fmap ((+1) . (+1)) z
+z3 = fmap (+1) . fmap (+1) $ z
+
+{-- Contravariant Functors --}
+
+{--
+class Contravariant (f :: * -> *) where
+    contramap :: (a -> b) -> (f b -> f a)
+--}
+
+-- contramap (g . f) = contramap f . contramap g
+
+-- newtype Predicate a = Predicate {getPredicate :: a -> Bool}
+
+-- instance Contravariant (Predicate a) where
+--  contramap (a -> b) -> (Predicate b -> Predicate a)    
+--  contramap f (Predicate p) = Predicate (p . f)
+
+w1 = contramap (length :: [a] -> Int) (Predicate even)
+w2 = getPredicate w1 [1..5]
+
+{-- Bifunctors --}
+
+{--
+class Bifunctor (p :: * -> * -> *) where
+    bimap :: (a -> b) -> (c -> d) -> p a c -> p b d
+--}
+
+-- covariant in both its arguments
+
+-- bimap id id = id
+-- bimap (g . f) (k . h) = bimap g k . bimap f h
+
+-- instance (Bifunctor (,)) where
+--  bimap :: (a -> b) -> (c -> d) -> (a, c) -> (b, d)
+--  bimap f g (x, y) = (f x, g y)
+
+w3 = bimap even (replicate 3) (4,5)
+
+
+--------------------------------------------------------------------
+{-- Applicative functors --}
+
+{--
+
+We have fmap :: (a -> b) -> f a -> f b, but is there a natural way to
+tranform it to fmap2 :: (a -> c -> b) -> f a -> f c -> f b? Say, for
+fmap2 (+) :: Maybe Int -> Maybe Int -> Maybe Int.
+Unfortunately, currying does not work:
+fmap (+) :: Maybe Int -> Maybe (Int -> Int)
+and we have no nice means to handle the value returned---we rather need
+something of the type Maybe (Int -> Int) -> Maybe Int -> Maybe Int here.
+Such a combinator could automatically give us fmap2, fmap3, etc.
+--}
+
+--class Functor f => Applicative f where
+--pure :: a -> f a
+--(<*>) :: f (a -> b) -> f a -> f b
+
+-- The laws:
+-- pure id <*> x = x
+-- pure (g $ x) = pure g <*> pure x
+---- What are the types of these terms?
+-- fg <*> pure x = pure ($ x) <*> fg
+-- fg <*> (fh <*> fx) = (pure (.) <*> fg  <*> fh) <*> fx
+
+---- This suffices for the functor axioms:
+-- fmap g fx = pure g <*> fx
+
+--instance Applicative Maybe where
+---- pure :: a -> Maybe a
+--pure = Just
+---- (<*>) :: Maybe (a -> b) -> Maybe a -> Maybe b
+--Nothing <*> _ = Nothing
+--(Just g) <*> fx = fmap g fx
+
+n3 = pure (+) <*> Just 2 <*> Just 3
+n3' = (+) <$> Just 2 <*> Just 3
+n4 = pure (+) <*> Just 2 <*> Nothing
+n4'= pure (+) <*> Nothing <*> Just 3
+
+
+--instance Applicative [] where
+---- pure :: a -> [a]
+--pure x = [x]
+---- (<*>) :: [a -> b] -> [a] -> [b]
+--gs <*> xs = [g x | g <- gs, x <- xs]
+
+n5 = pure (+1) <*> [1,2,3]
+n6 = [(+1),(+2),(+3)] <*> [1,2,3]
+n7 = pure (*) <*> [1,2,3] <*> [4,5]
+
+---- import Control.Applicative
+-- liftA, liftA2,...
+
+cartsq :: [a] -> [(a,a)]
+cartsq xs = (,) <$> xs <*> xs
+
+--
+
+newtype ZipList a = Z { getZipList :: [a] }
+    deriving Show
+
+instance Functor ZipList where
+--    fmap :: (a -> b) -> ZipList a -> ZipList b
+    fmap g (Z xs) = Z (map g xs)
+
+instance Applicative ZipList where
+--    pure :: a -> ZipList a
+    pure x = Z (repeat x)
+
+--    (<*>) :: ZipList (a -> b) -> ZipList a -> ZipList b
+    (<*>) (Z gs) (Z xs) = Z (map (\(u,v) -> u v) (zip gs xs))
+
+-- Do the applicative laws hold for ZipList?
+
+map' :: (a -> b) -> ZipList a -> ZipList b
+map' f xs = pure f <*> xs
+
+--getZipList :: ZipList a -> [a]
+--getZipList (Z xs) = xs
+
+scsum :: [Int] -> [Int] -> Int
+scsum xs ys = sum . getZipList $ (*) <$> Z xs <*> Z ys
+
+
+--instance Applicative ((->) a) where
+---- pure :: b -> (a -> b)
+---- <*> :: (a -> b -> c) -> (a -> b) -> (a -> c)
+--   pure x = \_ -> x
+--   h <*> g = \x -> h x (g x)
+
+x10 = pure (+) <*> (*3)
+--x10 = \x -> (+) (x*3)
+x11 = x10 <*> (^2)
+--x11 = \y -> (+) (y*3) (y^2)
+n8 = x11 $ 5
+
+
+--instance Applicative IO where
+---- pure :: a -> IO a
+--pure = return
+---- (<*>) :: IO (a -> b) -> IO a -> IO b
+--mg <*> mx = do {g <- mg; x <- mx; return (g x)}
+
+getChars :: Int -> IO String
+getChars 0 = return []
+getChars n = pure (:) <*> getChar <*> getChars (n-1)
+
+t3 = (++) <$> getLine <*> getLine
+-- What is the effect of the following?
+-- (try t4 with join from Control.Monad as well)
+t4 = putStrLn <$> getLine
+t4' = t4 >>= id
+-- So monads are necessary for IO at least...
+
+-- What if we try emulating bind with <*>?
+
+mybind' :: (Monad m) => m a -> (a -> m b) -> m (m b)
+mybind' mx g = pure g <*> mx
+
+mybind :: (Monad m) => m a -> (a -> m b) -> m b
+mybind mx g = join ( mybind' mx g )
+--mybind mx g = mybind' mx g >>= id
+
+{-- sequenceA --}
+
+sequenceA' :: Applicative f => [f a] -> f [a]
+--sequenceA' = foldr (liftA2 (:)) (pure [])
+sequenceA' [] = pure []
+sequenceA' (fx : fxs) = pure (:) <*> fx <*> sequenceA' fxs
+
+x12 = sequenceA [Just 1, Just 2, Just 3]
+x12' = sequenceA' [Just 1, Just 2, Just 3]
+x13 = sequenceA [Just 1, Nothing, Just 3]
+
+x14 = sequenceA [[1,2,3]]
+x15 = sequenceA [[1,2,3],[4,5]]
+x15' = sequenceA [[1,2,3],[],[4,5]]
+
+x16 = sequenceA [[3]]
+x17 = sequenceA [[2,3],[3]]
+x18 = sequenceA [[1,2,3],[2,3],[3]]
+
+cartsq' :: [a] -> [(a,a)]
+cartsq' xs = map (\[u,v] -> (u, v)) $ sequenceA [xs, xs]
+
+x19 = sequenceA [(+1),(*2),(^3)] $ 2
+
+x20 = sequenceA [Z [1,2,3], Z [4, 5], Z [6, 7]]
+--cf. zip3
+
+mzip :: [[a]] -> [[a]]
+mzip xss = getZipList . sequenceA $ map Z xss
+
+
+t5 = sequenceA [getChar,getChar]
+t5'= sequence [getChar,getChar]
+
+{-- Monads. --}
+
+--class Applicative m => Monad m where
+--    return :: a -> m a
+--    (>>=) :: m a -> (a -> m b) -> m b
+--    return = pure
+
+-- return x >>= f = f x 
+-- mx >>= return = mx
+-- (mx >>= f) >>= g = mx >>= (\x -> (f x >>= g))
+
+--instance Monad Maybe where
+---- (>>=) :: Maybe a -> (a -> Maybe b) -> Maybe b
+--  Nothing >>= _ = Nothing
+--  Just x >>= g = g x
+
+n9 = (+) <$> Just 2 <*> Just 3
+n9' = (+) <$> Just 2 <*> Nothing
+n9'' = (+) <$>  Nothing <*> Just 3
+
+n10 = div <$> Just 5 <*> Just 2
+n10' = div <$> Just 5 <*> Nothing
+
+n11 = div <$> Just 5 <*> Just 0
+
+sdiv :: Int -> Int -> Maybe Int
+sdiv n m | m == 0       = Nothing
+         | otherwise    = Just $ div n m
+
+n12 = sdiv <$> Just 5 <*> Just 0
+
+n13 = div  5 (div 12 6)
+
+n14 = div <$> Just 5 <*> (div <$> Just 12 <*> Just 6)
+
+n15 = sdiv <$> Just 12 <*> Just 0
+--n16' = sdiv <$> Just 5 <*> n15
+n16 = (sdiv <$> Just 5 <*> (n15 >>= id)) >>= id
+n16'' = join (sdiv <$> Just 5 <*> join n15)
+
+n17 = do mx <- n15
+         x <- mx
+         sdiv 5 x
+
+n18 = n15 >>= (\mx -> mx >>= \x -> sdiv 5 x)
+
+mybind1 :: (Monad m) => m a -> (a -> m b) -> m b
+mybind1 mx g = join ( pure g <*> mx )
+
+myjoin1 :: (Monad m) => m (m a) -> m a
+myjoin1 mmx = mmx >>= id
+
+myjoin2 mmx = do mx <- mmx
+                 mx
+
+t6 = join Nothing
+
+----
+
+--instance Monad [] where
+---- (>>=) :: [a] -> (a -> [b]) -> [b]
+--    xs >>= g = [y | x <- xs, y <- g x]
+
+z4 = [1,2,3] >>= (replicate 5)
+
+z5 = [[1,2],[],[3,4,5],[6]] >>= id
+
+concat' :: [[a]] -> [a]
+concat' = join
+
+filter' :: (a -> Bool) -> [a] -> [a]
+filter' p xs = xs >>= \x -> if p x then [x] else []
+
+z6 = filter' even [1,2,4,5,6,7,11]
+
+z7 = [y | y <- [1,2,4,5,6,7,11], even y]
+
+z8 = [1,2,3] >> [4,5]
+z8' = "qwerty" >> [4,5]
+z8'' = [()] >> [4,5]
+z8''' = [(),()] >> [4,5]
+
+-- guard
+
+filter'' p xs = xs >>= (\x -> guard (p x) >> return x)
+z9 = filter'' even [1,2,4,5,6,7,11]
+
+z9' = do x <- [1,2,4,5,6,7,11]
+         guard (even x)
+         return x
+
+sdiv' :: Int -> Int -> Maybe Int
+sdiv' n m = do guard (m /= 0)
+               return $ div n m
+               
+-- when
+
+sdiv'' n m = do when (m == 0) (putStrLn "Exception ahead!!!")
+                return $ div n m
+
+--
+
+filterM' :: Monad m => (a -> m Bool) -> [a] -> m [a]
+filterM' p [] = return []
+filterM' p (x:xs) = do b <- p x
+                       ys <- filterM' p xs
+                       return (if b then x:ys else ys)
+
+z10 = [[2,3],[5],[1,2],[3],[4,7],[1]] :: [[Int]]
+
+shead :: [a] -> Maybe a
+shead xs = do guard $ not (null xs)
+              return $ head xs
+
+
+evenheads :: [[Int]] -> Maybe [[Int]]
+evenheads = filterM (\xs -> even <$> shead xs)
+
+powset :: [a] -> [[a]]
+powset = filterM (\x -> [True,False])
+
+--
+
+mapM' :: Monad m => (a -> m b) -> [a] -> m [b]
+mapM' f [] = return []
+mapM' f (x:xs) = do y <- f x
+                    ys <- mapM' f xs
+                    return (y:ys)
+
+question :: Int -> IO Int
+question n = do putStrLn $ "Please square the number "
+                        ++ show n
+                s <- getLine
+                return (read s)
+
+z11 = [3,4,(-7),2,11] :: [Int]
+
+quiz :: [Int] -> IO Bool
+quiz ns = let test = and . zipWith (==) (map (^2) ns) in
+          fmap test $ mapM question ns
+
+{-- ap and liftM --}
+
+liftM' :: Monad m => (a -> b) -> m a -> m b
+liftM' f mx = mx >>= return . f
+
+l1 = liftM (^2) [1,2,3]
+l2 = liftM' (^2) [1,2,3]
+l3 = fmap (^2) [1,2,3]
+
+{- Deduce the functor laws for liftM' from 
+ - the monad laws for >>= and return.
+ -}
+
+ap' :: Monad m => m (a -> b) -> m a -> m b
+ap' mf mx = mf >>= \f -> (mx >>= return . f)
+
+l4 = ap [(+1),(^2)] [1,2,3]
+l5 = ap' [(+1),(^2)] [1,2,3]
+l6 = [(+1),(^2)] <*> [1,2,3]
+
+
---
+++ hse-fp-s5.hs
@@ -0,0 +1,181 @@
+import Data.List
+
+{-- Folds and Lazyness --}
+
+-- x1 is polymorphic
+x1 = undefined
+
+-- so x2 is Bool
+x2 = undefined || True 
+x3 = True || undefined
+-- (||) is strict in its first argument
+
+x2' = undefined * 0
+-- (*) is strict in BOTH arguments
+x3' = 0 * undefined
+
+x4 = (\_ -> 2021) undefined
+
+x5 = [True, False] ++ repeat undefined
+x6 = take 2 x5
+x7 = take 3 x5
+
+x8 = foldr (||) False x5
+x8' = or x5
+
+-- foldr f u (x:xs) = f x (foldr f u xs)
+-- x8 --> True || foldr (||) False [False, undefined..] --> True || (False || foldr ...)
+--             |_______--another way---> True
+
+-- let's MAKE (||) strict in its 2nd arg.:
+a |!| b = seq b (a || b) 
+-- a common context for seq is as follows:
+-- f_strict x = seq x (f x)
+y1 = seq undefined ((\_ -> 3) undefined)
+-- this is abbreviated to ($!):
+y2 = (\_ -> 3) $! undefined
+-- f $! x = seq x (f x)
+
+x8a = foldr (|!|) False x5
+-- stack overflow, no 'undefined' exception!
+-- x8a --> True |!| (foldr (|!|) False [False, undefined..]) -->
+--                  True |!| (False |!| (foldr (|!|) False [undefined..]) --> 
+--                            True |!| (False |!| (undefined |!| (foldr (|!|) False [undefined..])) --> 
+--                                          True |!| (False |!| (undefined |!| (undefined |!| (foldr (|!|) False [undefined..])))                                                   --> ...
+-- STACK:  |!| True |!| True  |!| True      |!| True          ...
+--                  |!| False |!| False     |!| False
+--                            |!| undefined |!| undefined
+--                                          |!| undefined
+                                      
+x8b = foldr (|!|) False $ x7
+-- is this any better?
+-- x8b -->> True |!| (False |!| (undefined |!| False)) --reduce the 2nd arg-->>  True |!| (False |!| undefined) -->> undefined
+
+
+-- foldl f u (x:xs) = foldl f (f u x) xs
+-- this uses all the memory, yet gives NO stack overflow
+x9 = foldl (||) False x5
+-- x9 -->  foldl (||) (False || True) [False,undefined..] --> foldl (||) ((False || True) || False) [undefined..] --> ... 
+-- nothing goes to foldl function stack (as foldl is tail-recursive, it CANNOT cause stack overflow on itself)
+x9a = foldl (|!|) False x5
+-- the same here despite (|!|) strictness, since the first arg of foldl is never evaluated
+
+-- What if we make foldl strict in the first argument?
+-- foldl' f u (x:xs) = seq u (foldl' f (f u x) xs)
+x9b = foldl' (||) False x5
+-- ALMOST the same -- as we have
+-- foldl' (||) (((False || True) || False) || undefined) [undefined..] -->> foldl' (||) True [undefined..] --> foldl' (||) True [undefined..]
+-- a genuine infinite loop on bounded memory, hence NO excessive memory usage
+
+-- let's combine it with strict (|!|)
+x9c = foldl' (|!|) False x5
+--  foldl (|!|) (((False |!| True) |!| False) |!| undefined) [undefined..] -->> foldl (|!|) undefined [undefined..] --> EXCEPTION
+
+------
+-- As for now, we see how strictness influence folds. Let's look at more practical things.
+------
+
+-- 100000000 for my desktop
+bigList = [1..20000000]
+
+-- let's sum it up...
+x10 = foldr (+) 0 bigList
+-- (+) is strict in both arguments
+-- x10 -->> 0 + (1 + (2 + (foldr (+) 0 [3..100000000] ))) -> ...
+--STACK:    0+   0+   0+
+--               1+   1+
+--                    2+
+--
+-- stack overflow!
+
+
+x11 = foldl (+) 0 bigList
+-- first, great memory usage (about 10G yet still bounded)
+-- THEN stack overflow!
+-- Indeed,
+-- x11 -->> foldl (+) (((((0 + 1) + 2) + 3) + 4) + ... + 99999999) [100000000] -->> ((((0 + 1) + 2) + 3) + 4) + ... )) + 100000000
+--we need lots of space to hold this expression; now it is being evaluated:
+--STACK: +100000000 +100000000 +100000000 
+--                  +99999999  +99999999
+--                             +99999998
+-- this can fit the heap yet is too large for the stack!
+
+
+-- But we can reduce the first argument at every foldl recursion step!
+-- foldl' does exactly that:
+x12 = foldl' (+) 0 bigList
+-- x11 --> foldl' (+) (0 + 1) [2..100000000] --> foldl' (+) 1 [2..100000000] --> foldl' (+) (1 + 2) [3..100000000] --> 
+--  foldl' (+) (3) [4..100000000] --> ...
+--  No need in a huge memory footprint!
+
+{-- Practical considerations --}
+
+-- Is foldl' always better than foldl?
+-- ALMOST!
+
+-- But there may be exceptions!
+-- Let's make (||) lazy in its FIRST argument but strict in the second one:
+a \| b = flip (||) a b
+
+x13 = foldl' (\|) False [undefined, True]
+-- x13 --> foldl' (\|) (False \| undefined) [True] --> foldl' (\|) undefined [True] --> EXCEPTION
+x14 = foldl (\|) False [undefined, True]
+-- x14 --> foldl (\|) (False \| undefined) [True] --> foldl (\|) ((False \| undefined) \| True) [] --> 
+--   ((False \| undefined) \| True) --> True
+
+-- Is foldl' always better than foldr?
+-- NO WAY!
+
+-- foldr is the genuine structural recursion on lists and is capable of proceeding infinite lists (unlike foldl and foldl').
+
+-- foldl implicitly reverses a list unlike foldr:
+x15 = foldl (flip (:)) [] "qwerty"
+x16 = foldr (:) [] "qwerty"
+
+
+{-- Efficient recursion tricks --}
+
+-- memoize and reuse!
+
+-- Binomial coefficients
+-- Exp. time in n -- very inefficient
+binom :: Int -> Int -> Integer
+binom _ 0 = 1
+binom 0 _ = 0
+binom n k = binom (n - 1)  k + binom (n - 1) (k - 1)
+-- the resulting tree of recursive calls may contain identical values multiple times;
+-- it would be beneficial to store them for further usage ('memoization')
+
+-- let's sequentially compute Pascal's Triangle rows!
+-- C_n^k = C_{n-1}^{k-1} + C_{n-1}^k = C_{n-1}^{k-1} + ( C_{n-2}^{k-1} + C_{n-2}^k ) = ... = C_{n-1}^{k-1} + ... + C_{k-1}^{k-1}
+-- So, (C_n^k)_{n >= k} is the sequence of partial sums for (C_n^{k-1})_{n >= k - 1}
+binom' :: Int -> Int -> Integer
+binom' _ 0 = 1
+binom' 0 _ = 0
+binom' n k =  ((iterate (scanl1 (+)) (replicate n 1)) !! k) !! (n - k)
+
+
+-- Catalan numbers -- let's apply a more general approach
+-- a simplistic inefficient implementation is based on the formula C(n) = sum(C(i) * C(n - 1 - i), i = 0..n-1)
+catalan :: Int -> Integer
+catalan n | n == 0      = 1
+          | otherwise   = sum [ catalan i * catalan (n - 1 - i)
+                                | i <- [0 .. n-1] ]
+-- this is clearly exponential in space and time
+
+-- now, let us memoize all the prevoius values while applying the same naive formula
+catalan' :: Int -> Integer
+catalan' n = table !! n
+             where table = map f [0..n]
+                   f m = if m == 0 then 1 else
+                            sum [ table !! i * table !! (m - 1 - i)
+                                  | i <- [0 .. m-1] ]
+
+-- How does this work?
+-- table  -->> [1, table !! 0, table !! 0 * table !! 1 + table !! 1 * table !! 0, ..., table !! 0 * table !! (n-1) + ...]
+-- This list is finite and may take space from O(n log n) (for normal forms) to  O(n^3 log n) (for thunks).
+-- Practically, the space consumed is close to O(n log n) since each thunk is evaluated at its first usage and
+-- then just the value is stored.
+
+
+
